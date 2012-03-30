251becf53042e918ced6ca8efea86ffdbc1209c6
Tim Richards <tim.d.richards@gmail.com>
2012-03-30 12:35:06 -0400

    Added server.c

---
+++ server.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int handler(int sig) {
+
+}
+
+void do_random_work() {
+  static int flag = 0;
+  if (!flag) {
+    
+  }
+  int r = 
+}
+
+void service_req() {
+
+}
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
